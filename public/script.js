8b06960e1d691dad5b24961967d6f900d16f219a
Vijayabaskar <2020itc040@maduracollege.edu.in>
2023-08-22 00:15:30 +0530

    emoji & cmd

--- public/script.js
+++ public/script.js
@@ -18,15 +18,54 @@ document.addEventListener('DOMContentLoaded', () => {
         socket.emit('set contact', contactName); // Emit contact name to server
     }
 
+    function clearMessages() {
+        while (messages.firstChild) {
+            messages.removeChild(messages.firstChild);
+        }
+    }
+
+    const emojiMapping = new Map([
+        ['react', '⚛️'],
+        ['woah', '😯'],
+        ['hey', '👋🏼'],
+        ['lol', '😂'],
+        ['like', '❤️'],
+        ['congratulation', '🥳'],
+    ]);
+    
+    const commandMapping = new Map([
+        ['/help', () => {
+            alert("List of Available Commands! \n -/hlep \n -/clear \n -/reload");
+        }],
+        ['/clear', () => {
+            clearMessages();        
+        }],
+        ['/reload', () => {
+            location.reload();
+        }]
+    ])
+
 
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        const message = input.value.trim();
-        if (message !== '') {
-            socket.emit('chat message', message);
+        const sentence = input.value.trim().toLowerCase();
+        let cmd = commandMapping.get(sentence)
+        console.log(cmd);
+        const words = sentence.split(' ');
+
+
+        let msg = words.map(word => emojiMapping.get(word) || word).join(' ')
+
+        if(cmd) {
+            cmd();
             input.value = '';
         }
+        else if (msg !== '') {
+            socket.emit('chat message', msg);
+            input.value = '';
+        } 
+
     });
 
     socket.on('chat message', (msg) => {
